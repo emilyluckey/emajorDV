547ac3a013ce1ab249e80ed500f7175a7ca39012
Martin老師 <tpemartin@gmail.com>
2020-08-27 22:41:03 +0800

    add create_snapshot function

--- R/svg.R
+++ R/svg.R
@@ -1,3 +1,30 @@
+#' Create snapshot from SVG
+#'
+#' @param svgfilename A character. file path of the svg file
+#' @param width A character. default="100" px
+#' @param destfolder A character. default="./snapshot"
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+create_snapshot <- function(svgfilename, width="100", destfolder="./snapshot"){
+  oneSVG <- magick::image_read_svg(svgfilename)
+  # magick::image_info(oneSVG) -> metaSVG
+  # asratio <- with(metaSVG, width/height)
+  magick::image_scale(
+    oneSVG,
+    width
+  ) -> smallSVG
+  saveFilename <-
+    basename(svgfilename) %>%
+    stringr::str_replace(".svg$",".png")
+  magick::image_write(
+    smallSVG, path=file.path(destfolder,saveFilename),
+    format="png"
+  )
+  message(file.path(destfolder,saveFilename))
+}
 #' Launch Boxy SVG and open filename inside
 #'
 #' @param filename
