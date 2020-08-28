4575061b3f129892a96af078c6335ca863245a69
Martin老師 <tpemartin@gmail.com>
2020-08-28 10:46:37 +0800

    add web service

--- R/web.R
+++ R/web.R
@@ -22,3 +22,41 @@ create_jqueryPage <- function(){
   file.edit("index.html")
 }
 
+#' serve the last modified html (usually the latest knitted Rmd)
+#'
+#' @param path A character, default="."
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+serveTheLastModified <- function(path="."){
+  rootPath <- normalizePath(".")
+  if(length(servr::daemon_list())==0){
+    servr::httd()
+  }
+  servr::server_config() -> sconfig
+  sconfig$host -> host
+  sconfig$port -> port
+  list.files(path=path,pattern=".html$", full.names = T) -> allHtmls
+  file.info(allHtmls) -> allHtmlsInfo
+  with(allHtmlsInfo, which(mtime==max(mtime)))-> loc_theLatest
+  stringr::str_remove(allHtmls[[loc_theLatest]],
+                      glue::glue("^(.|{rootPath})")) -> html2open
+  with(out, paste0("http://", host,":",port,html2open)) -> url0
+  browseURL(url0)
+}
+
+#' Create web service
+#'
+#' @return
+#' @export
+#'
+#' @examples none
+webService <- function(){
+  service <- new.env(parent=globalenv())
+  service$serveTheLastModified <- serveTheLastModified
+  service$create_jqueryPage <- create_jqueryPage
+  service$browse_last <- servr::browse_last
+  service
+}
